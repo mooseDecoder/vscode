f7f0f0b2995455db1a58f81a58be22c0c9aeed95
thalassau <thalassau@thalassaus-mbp.attlocal.net>
2018-04-30 21:16:13 -0400

    more eb methods

--- Ch3.cs
+++ Ch3.cs
@@ -1,7 +1,7 @@
 using System;
 namespace vscode
 {
-    public static class Ch3
+    public  class Ch3
     {
         public static void Run()
         {
@@ -9,9 +9,12 @@ namespace vscode
             ExpressionBodiedWithParm(44);
         }
 
+        
         public static void ExpressionBodied() => 
             Console.WriteLine("Here is an example of an Expression Bodied Method.");
         public static void ExpressionBodiedWithParm(int z) =>
-            Console.WriteLine("Here is an expression bodied method. Note only one statement: {0}", z);
+            Console.WriteLine(format: "Here is an expression bodied method. parm: {0}", arg0: z);
+        
+        
     }
 }
